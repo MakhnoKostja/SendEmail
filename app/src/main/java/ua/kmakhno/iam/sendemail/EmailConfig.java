ad80b13c77bd3c5fddeba2c50ff22b5d1b379214
Kostja Makhno <ichbindragau@gmail.com>
2016-06-01 17:06:51 +0300

    email config

---
+++ app/src/main/java/ua/kmakhno/iam/sendemail/EmailConfig.java
@@ -0,0 +1,10 @@
+package ua.kmakhno.iam.sendemail;
+
+/**
+ * Created by I am on 01.06.2016.
+ */
+public class EmailConfig {
+
+   public final static String EMAIL = "Ваш Емайл";
+    public final static String PASSWORD = "Ваш пароль";
+}
